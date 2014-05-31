1e93598061e7b53c275f94e7e8c1fad3d3e08bfc
Gabriel Scholz <gabe.scholz@zozi.com>
2014-05-31 12:09:51 -0700

    Build

--- test/angular-gs-to-snake-case_spec.js
+++ test/angular-gs-to-snake-case_spec.js
@@ -1,22 +1,32 @@
 describe('angular-gs-to-snake-case', function () {
   beforeEach(module('gs.to-snake-case'));
 
-  var $scope;
+  var toSnakeCase;
 
-  beforeEach(inject(function ($rootScope) {
-    $scope = $rootScope.$new();
+  beforeEach(inject(function (_toSnakeCase_) {
+    toSnakeCase = _toSnakeCase_;
   }));
 
-  // happy path(s)
-  it('', function () {
+  it('converts camel case to snake case', function () {
+    var a = 'gabeIsCool',
+      b = 'GabeIsCool',
+      solution = 'gabe_is_cool';
+
+    expect(toSnakeCase(a)).toEqual(solution);
+    expect(toSnakeCase(b)).toEqual(solution);
   });
 
-  // null input path
-  it('', function () {
+  it('returns undefined if given undefined', function () {
+    expect(toSnakeCase(undefined)).toEqual(undefined);
   });
 
-  // wrong type path
-  it('', function () {
+  it('raises an exception', function () {
+
+    var throwMe = function () {
+      toSnakeCase([1,2,3]);
+    };
+
+    expect(throwMe).toThrow();
   });
 
 });
